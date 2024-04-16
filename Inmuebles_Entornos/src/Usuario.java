21177c31814c3999a6bb560a265af47dc692e7c3
Ander Arregui <ander.arregui@ikasle.egibide.org>
2024-04-16 12:34:46 +0200

    usuario clases

--- Inmuebles_Entornos/src/Usuario.java
+++ Inmuebles_Entornos/src/Usuario.java
@@ -1,3 +1,25 @@
 public class Usuario {
+        private String Dni;
+        private String Nombre;
 
+    public Usuario(String dni, String nombre) {
+        Dni = dni;
+        Nombre = nombre;
+    }
+
+    public String getDni() {
+        return Dni;
+    }
+
+    public void setDni(String dni) {
+        Dni = dni;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
 }
